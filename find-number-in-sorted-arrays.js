c324a0ba0c4c2ce9fd91787c77401c453349f77a
Vibes-ins <j893412899@outlook.com>
2021-07-16 20:56:15 +0800

    Add File

---
+++ find-number-in-sorted-arrays.js
@@ -0,0 +1,34 @@
+function search(nums, target) {
+    let count = 0
+    const index = binarySearch(nums, target)
+    for (let offset = 0; nums[index - offset] === target || nums[index + offset] === target; offset++) {
+        if (offset === 0 && nums[index - offset] === target) {
+            count++
+            continue
+        }
+        if (nums[index - offset] === target) {
+            count++
+        }
+        if (nums[index + offset] === target) {
+            count++
+        }
+    }
+    return count
+}
+
+function binarySearch(arr, val) {
+    let low = 0
+    let high = arr.length - 1
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2 )
+        if (val == arr[mid]) {
+            return mid
+        }
+        if (val > arr[mid]) {
+            low = mid + 1
+        } else if (val < arr[mid]) {
+            high = mid - 1
+        }
+    }
+    return -1
+}
