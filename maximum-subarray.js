73d4c33e7df0dc9af0abb2928239a607725ab674
INS <j893412899@outlook.com>
2021-08-08 21:36:06 +0800

    Update File

--- maximum-subarray.js
+++ maximum-subarray.js
@@ -1,13 +1,10 @@
 // https://leetcode-cn.com/problems/maximum-subarray/
 function maxSubArray(nums) {
-    if (nums.length <= 1) {
-        return nums[0] || 0
-    }
-    let dp = [ nums[0] ]
-    let max = dp[0]
+    let currentMax = nums[0]
+    let max = currentMax
     for (let i = 1; i < nums.length; i++) {
-        dp[i] = Math.max(dp[i - 1] + nums[i], nums[i])
-        max = Math.max(max, dp[i])
+        currentMax = Math.max(currentMax + nums[i], nums[i])
+        max = Math.max(currentMax, max)
     }
     return max
 }
